27a4dbb746fb5cb44d90057ce1943acfd19484b9
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-10 09:17:03 -0300

    feat: add express initial server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("iniciou mudou");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
+});
+
+app.listen(PORT, () => console.log("SErvido iniciado"));
