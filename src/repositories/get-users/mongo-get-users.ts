0538e9cd414032a5f44ea2b1085bb5c7f3dbdad9
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-10 11:09:12 -0300

    feat: ensure GetUserRepositories with mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,14 +1,17 @@
 import { IGetUserRepository } from "../../controllers/get-users/protocols";
+import { MondoDB } from "../../dataBase/mongodb";
+import { User } from "../../models/user";
 
 export class MongoGetUsersRepositories implements IGetUserRepository {
   async getUser() {
-    return [
-      {
-        firstName: "João",
-        lastName: "Eduardo",
-        email: "joao2gmail.com",
-        password: "eduj1234",
-      },
-    ];
+    const users = await MondoDB.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
